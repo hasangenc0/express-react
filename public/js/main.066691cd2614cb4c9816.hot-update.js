ddb3bffc11feaff17f28839716959c9a4f4608a1
hasan <hsnyng@outlook.com>
2018-11-13 22:24:55 +0300

    initial

---
+++ public/js/main.066691cd2614cb4c9816.hot-update.js
@@ -0,0 +1,5 @@
+webpackHotUpdate("main",{"./node_modules/css-loader/index.js!./src/css/App.css":
+/*!***************************************************!*\
+  !*** ./node_modules/css-loader!./src/css/App.css ***!
+  \***************************************************/
+/*! no static exports found */function(module,exports,__webpack_require__){eval('exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);\n// imports\n\n\n// module\nexports.push([module.i, ".App {\\n  margin: 1rem;\\n  font-family: Arial, Helvetica, sans-serif;\\n}\\n\\n.App a {\\n  color: white;\\n}\\n\\n.App a h6 {\\n  z-index: 9999;\\n}\\n\\n.App a:hover {\\n  text-decoration: none;\\n}\\n.App img {\\n  width: 300px;\\n  height: 210px;\\n}", ""]);\n\n// exports\n\n\n//# sourceURL=webpack:///./src/css/App.css?./node_modules/css-loader')}});
--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import React, { Component} from "react";
 import "./css/App.css";
 
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.linkedin = "https://www.linkedin.com/in/hasangenc0";
+    this.logo_url = "https://github.com/hasangenc0/express-react/raw/master/logo.png";
+    this.state = {    
+      logo : null,
+      logo_display : ".d-none",
+      class: "lds-dual-ring",
+    };
+  }
+
+  componentDidMount() {
+    const logo = new Image();
+
+    logo.src = this.logo_url;
+
+    logo.onload = () => {
+      this.setState({class: "" }); 
+      this.logo.setAttribute(
+        "style",
+        "background-image: url('" + this.logo_url + "')"
+      );
+    }
+
+  }
+
   render(){
-    return(
-      <div className="App">
-        <h1> Hello, Hasan World! </h1>
-      </div>
+    return(      
+      <center className="App">
+        <a href="https://github.com/hasangenc0/express-react">
+          <h1> Express - React </h1>
+          <div class={this.state.class} ref={logo => this.logo = logo}></div>          
+        </a>
+        <h6>Created by <a href={this.linkedin}> <b>Hasan Genc</b></a></h6>
+      </center>
     );
   }
 }
