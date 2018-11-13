7a36c38cf8053a358bd0ab1d291f418e6756ecbd
hasan <hsnyng@outlook.com>
2018-11-13 19:51:11 +0300

    initial

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,22 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
-var shell = require('./shellHelper');
+var child_process = require('child_process');
 
-shell.exec('webpack --watch', function(err){
-	console.log('executed test');
+// Starts webpack --watch command
+var cmd = 'webpack --watch';
+var parts = cmd.split(/\s+/g);
+var p = child_process.spawn(parts[0], parts.slice(1), {stdio: 'inherit'});
+p.on('exit', function(code){
+	var err = null;
+	if (code) {
+			err = new Error('command "'+ cmd +'" exited with wrong status code "'+ code +'"');
+			err.code = code;
+			err.cmd = cmd;
+	}
+	if (cb) cb(err);
 });
-
+		
 gulp.task('default', ['browser-sync'], function () {
 });
 
@@ -20,6 +30,7 @@ gulp.task('browser-sync', ['nodemon'], function() {
         port: 7000,
 	});
 });
+
 gulp.task('nodemon', function (cb) {
 	
 	var started = false;
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, sWorld! </h1>
+        <h1> Hello, Hasan World! </h1>
       </div>
     );
   }
